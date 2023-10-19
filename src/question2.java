a4980c1b93c624b81684b977692b76d32b05a7df
danukaj <damayanthiamarakoon753@gmail.com>
2023-10-19 10:11:55 +0530

    sum of numbers

--- src/question2.java
+++ src/question2.java
@@ -1,12 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author danuk
- */
+/**/
 public class question2 {
     
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 0; i <= 100; i++) {
+            total = total+i;
+        }
+        System.out.println("1-100 total is :"+total);
+    }
 }
